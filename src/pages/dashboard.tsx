ada6f9e77befda8d727f41864239c4d33ea83eae
pedrim01 <peuu.aguiar@gmail.com>
2023-06-11 19:43:57 -0300

    trabalhando na home page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,39 @@
+import { Header } from "@/components/Header";
+import { Sidebar } from "@/components/Sidebar";
+import useSidebar from "@/hook/useSidebar";
+import { useEffect, useRef } from "react";
+
+export default function Dashboard() {
+  const { isVisibleSidebar, setIsVisibleSidebar } = useSidebar();
+  const handleRefSidebar = useRef<HTMLDivElement>(null);
+  
+
+  
+  
+  const handler = (event: MouseEvent) => {
+    if (event.x>257 && isVisibleSidebar && !handleRefSidebar.current?.contains(event.target as Node | null)) {
+      // console.log(event);
+      // console.log(isVisibleSidebar)
+      setIsVisibleSidebar(!isVisibleSidebar);
+    }
+  };
+  useEffect(() => {
+  
+    document.addEventListener("click", handler);
+
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+
+  }, [isVisibleSidebar,setIsVisibleSidebar]);
+
+  return (
+    <div className="flex h-screen w-screen flex-col lg:flex-row">
+      <Sidebar refSidebar={handleRefSidebar} />
+
+      <div className="flex-1">
+        <Header />
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,69 @@
-import { Header } from "@/components/Header";
-import { Sidebar } from "@/components/Sidebar";
-import useSidebar from "@/hook/useSidebar";
-import { useEffect, useRef } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
-export default function Dashboard() {
-  const { isVisibleSidebar, setIsVisibleSidebar } = useSidebar();
-  const handleRefSidebar = useRef<HTMLDivElement>(null);
-  
+export default function Home() {
+  const [stateDisplay, setStateDisplay] = useState<"flex" | "hidden">("hidden");
 
-  
-  
-  const handler = (event: MouseEvent) => {
-    if (event.x>257 && isVisibleSidebar && !handleRefSidebar.current?.contains(event.target as Node | null)) {
-      // console.log(event);
-      // console.log(isVisibleSidebar)
-      setIsVisibleSidebar(!isVisibleSidebar);
-    }
-  };
+  function handleClickButtonTop() {
+    window.scrollTo(0, 0);
+  }
+
+  function handleScrollY() {
+    window.scrollY > 10 ? setStateDisplay("flex") : setStateDisplay("hidden");
+  }
+
   useEffect(() => {
-  
-    document.addEventListener("click", handler);
+    document.addEventListener("scroll", handleScrollY);
 
     return () => {
-      document.removeEventListener("click", handler);
+      document.removeEventListener("scroll", handleScrollY);
     };
-
-  }, [isVisibleSidebar,setIsVisibleSidebar]);
+  }, [stateDisplay, setStateDisplay]);
 
   return (
-    <div className="flex h-screen w-screen flex-col lg:flex-row">
-      <Sidebar refSidebar={handleRefSidebar} />
+    <>
+      <nav className="z-1 fixed left-0 right-0 top-0 w-full bg-gray-300">
+        <ul className="mr-4 flex justify-end gap-4">
+          <li><a href={"#about"}>Sobre</a></li>
+          <li><a href={"#plans"}>Planos</a></li>
+          <li><a href={"#tools"}>Ferramentas</a></li>
+          <li><a href={"#contact"}>Contato</a></li>
+        </ul>
+      </nav>
 
-      <div className="flex-1">
-        <Header />
-      </div>
-    </div>
+      <main>
+        <section id="signIn" className="h-screen bg-green-400 pt-5">
+          Login
+        </section>
+
+        <section id="about" className="min-h-screen bg-red-400 pt-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, hic ipsum? Voluptatum unde provident officia quam magnam
+          accusamus. Mollitia omnis illo eligendi veniam itaque facilis neque tempore natus molestiae. Voluptatibus. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Expedita, hic ipsum? Voluptatum unde provident officia quam magnam accusamus. Mollitia omnis
+        </section>
+
+        <section id="plans" className="min-h-screen bg-blue-400 pt-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, hic ipsum? Voluptatum unde provident officia quam magnam
+          accusamus. Mollitia omnis illo eligendi veniam itaque facilis neque tempore natus molestiae. Voluptatibus. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Expedita, hic ipsum? Voluptatum unde provident officia quam magnam accusamus. Mollitia omnis
+          illo eligendi veniam itaque facilis neque tempore natus molestiae. Voluptatibus. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Expedita, hic ipsum? Voluptatum unde provident officia quam magnam accusamus. Mollitia omnis illo eligendi
+          veniam itaque facilis neque tempore natus molestiae. Voluptatibus. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Expedita, hic ipsum? Voluptatum unde provident officia quam magnam accusamus. Mollitia omnis illo eligendi veniam itaque facilis
+        </section>
+
+        <section id="tools" className="min-h-screen bg-gray-400 pt-5">
+          Ferramentas
+        </section>
+
+        <footer id="contact" className="min-h-screen bg-cyan-400 pt-5">
+          Contato
+        </footer>
+      </main>
+
+      <button className={`${stateDisplay} z-2 fixed bottom-5 right-5 bg-white text-black`} onClick={handleClickButtonTop}>
+        Topo
+      </button>
+    </>
   );
 }
