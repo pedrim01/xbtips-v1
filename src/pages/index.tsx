0e672899991fb366421f85ef99e04f0a250fc7e8
pedrim01 <peuu.aguiar@gmail.com>
2023-06-19 22:00:46 -0300

    finalizando landingpage principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { Link as LinkScrool } from "react-scroll";
+
+import Link from "next/link";
+import {BsChevronRight} from 'react-icons/bs'
 import { TiChevronRightOutline } from "react-icons/ti";
 
 import { useEffect, useState } from "react";
-import Link from "next/link";
+
 import { Logo } from "@/components/Logo";
 import { TbUser } from "react-icons/tb";
 import Button from "@/components/Button";
@@ -12,18 +16,20 @@ import useAuthFirebase from "@/hook/useAuthFirebase";
 export default function Home() {
   const [stateDisplay, setStateDisplay] = useState<"flex" | "hidden">("hidden");
   const [stateMenu, setStateMenu] = useState(false);
-  const {user} = useAuthFirebase()
+  const { user } = useAuthFirebase();
 
   function handleClickButtonTop() {
-    window.scrollTo(0, 0);
-    window.location.href = "/";
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   }
   function handleClickButtonMenu() {
     setStateMenu(!stateMenu);
   }
 
   function handleScrollY() {
-    window.scrollY > 10 ? setStateDisplay("flex") : setStateDisplay("hidden");
+    window.scrollY > 100 ? setStateDisplay("flex") : setStateDisplay("hidden");
   }
 
   useEffect(() => {
@@ -32,7 +38,7 @@ export default function Home() {
     return () => {
       document.removeEventListener("scroll", handleScrollY);
     };
-  }, [stateDisplay]);
+  }, []);
 
   return (
     <>
@@ -87,18 +93,21 @@ export default function Home() {
             href={"/login"}
           /> */}
 
-          {user ? <Button
-            className="flex items-center gap-1 hover:text-white"
-            Icon={<TbUser className="text-yellow-500" />}
-            href={"/dashboard"}
-            value={`${user.name}`}
-          /> : <Button
-          className="flex items-center gap-1 hover:text-white"
-          Icon={<TbUser className="text-yellow-500" />}
-          href={!user ? "/login" : "/dashboard"}
-          value={"Entrar"}
-        /> }
-          
+          {user ? (
+            <Button
+              className="flex items-center gap-1 hover:text-white"
+              Icon={<TbUser className="text-yellow-500" />}
+              href={"/dashboard"}
+              value={`${user.name}`}
+            />
+          ) : (
+            <Button
+              className="flex items-center gap-1 hover:text-white"
+              Icon={<TbUser className="text-yellow-500" />}
+              href={!user ? "/login" : "/dashboard"}
+              value={"Entrar"}
+            />
+          )}
 
           <Button
             className="rounded-md border border-yellow-500 px-3 py-1 duration-500 ease-in-out hover:bg-yellow-500 hover:text-white"
@@ -113,34 +122,55 @@ export default function Home() {
           ${stateMenu ? "fixed flex w-full" : "hidden"} 
           mt-20 flex-col items-start justify-center gap-4 bg-zinc-800 p-4 text-xl font-semibold text-zinc-400 lg:hidden`}
       >
-        <li onClick={handleClickButtonMenu}>
-          <a href={"#home"} className="hover:text-white">
-            Home
-          </a>
-        </li>
+        <LinkScrool
+          to={"home"}
+          onClick={handleClickButtonMenu}
+          className="py-2 px-4 flex items-center w-1/2 justify-around rounded-md cursor-pointer duration-300 ease-in-out hover:bg-zinc-900 hover:text-white"
+        >
+          <span className="flex-1"> Home</span> 
+          <BsChevronRight className="text-sm tracking-tighter"/>
+        </LinkScrool>
 
-        <li onClick={handleClickButtonMenu}>
-          <a href={"#about"} className="hover:text-white">
-            Sobre
-          </a>
-        </li>
 
-        <li onClick={handleClickButtonMenu}>
-          <a href={"#plans"} className="hover:text-white">
-            {" "}
-            Planos
-          </a>
-        </li>
-        <li onClick={handleClickButtonMenu}>
-          <a href={"#tools"} className="hover:text-white">
-            Ferramentas
-          </a>
-        </li>
-        <li onClick={handleClickButtonMenu}>
-          <a href={"#contact"} className="hover:text-white">
-            Contato
-          </a>
-        </li>
+        <LinkScrool
+          to={"about"}
+          onClick={handleClickButtonMenu}
+          className="py-2 px-4 flex items-center w-1/2 justify-around rounded-md cursor-pointer duration-300 ease-in-out hover:bg-zinc-900 hover:text-white"
+        >
+          <span className="flex-1"> Sobre</span> 
+          
+          <BsChevronRight className="text-sm tracking-tighter"/>
+        </LinkScrool>
+
+        <LinkScrool
+          onClick={handleClickButtonMenu}
+          className="py-2 px-4 flex items-center w-1/2 justify-around rounded-md cursor-pointer duration-300 ease-in-out hover:bg-zinc-900 hover:text-white"
+          to={"plans"}
+        >
+          <span className="flex-1"> Plans</span> 
+          
+          <BsChevronRight className="text-sm tracking-tighter"/>
+        </LinkScrool>
+
+        <LinkScrool
+          onClick={handleClickButtonMenu}
+          className="py-2 px-4 flex items-center w-1/2 justify-around rounded-md cursor-pointer duration-300 ease-in-out hover:bg-zinc-900 hover:text-white"
+          to={"tools"}
+        >
+          <span className="flex-1"> Ferramentas</span> 
+          
+          <BsChevronRight className="text-sm tracking-tighter"/>
+        </LinkScrool>
+
+        <LinkScrool
+          onClick={handleClickButtonMenu}
+          className="py-2 px-4 flex items-center w-1/2 justify-around rounded-md cursor-pointer duration-300 ease-in-out hover:bg-zinc-900 hover:text-white"
+          to={"contact"}
+        >
+          <span className="flex-1"> Contato</span> 
+          
+          <BsChevronRight className="text-sm tracking-tighter"/>
+        </LinkScrool>
       </ul>
 
       <main>
